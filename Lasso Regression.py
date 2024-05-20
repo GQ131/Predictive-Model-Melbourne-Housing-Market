a66345c7aed5b8c2e1d85baf93fb3c6dcfbf4e09
Gustavo Quintero <148562622+GQ131@users.noreply.github.com>
2024-05-20 11:08:00 -0700

    Update Lasso Regression.py

--- Lasso Regression.py
+++ Lasso Regression.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from scipy import stats
+ scipy import stats
 from scipy.stats import norm, skew
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
